736e820562ad766eb26a4f437bc8ac7d33ac1675
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2020-08-03 15:37:40 +0530

    Updates

---
+++ precache-manifest.2f37a556814a680103af6bfed03a4e66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2dbb398cc3d846204f96dc0bdd41ed3",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "3ab6926d4cc5bc3f1869",
+    "url": "/Tic-Tac-Toe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b4619adcea566cd6e11c",
+    "url": "/Tic-Tac-Toe/static/css/main.6b16821c.chunk.css"
+  },
+  {
+    "revision": "3ab6926d4cc5bc3f1869",
+    "url": "/Tic-Tac-Toe/static/js/2.dfbf6b16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.dfbf6b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4619adcea566cd6e11c",
+    "url": "/Tic-Tac-Toe/static/js/main.bf41dd38.chunk.js"
+  },
+  {
+    "revision": "ab422492a78b74a08c57",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.05041f24.js"
+  }
+]);
